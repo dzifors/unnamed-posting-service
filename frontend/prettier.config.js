3dd948f9445ab3dbe286eed36bf8d2eea1977916
dzifors <83210112+dzifors@users.noreply.github.com>
2024-10-08 16:23:14 +0200

    Add authentication using Clerk

---
+++ frontend/prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type import {Config} from "prettier"
+ */
+const options = {
+    arrowParens: "always",
+    singleQuote: false,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    semi: true,
+    tabWidth: 4,
+    trailingComma: "none"
+};
+module.exports = options;
